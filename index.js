b6f49efe029202cf443b852d4e2312192594d417
Will Kelly <willkelly9617@gmail.com>
2021-03-26 15:34:37 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+let hiddenQ = document.querySelectorAll('[data-role = "revealAnswer"]');
+
+hiddenQ.forEach((q) =>
+  q.addEventListener('click', (e) => {
+    e.target.previousElementSibling.classList.toggle('answerRevealed');
+  })
+);
+
+let keyframeEx = document.querySelector('.slide');
+
+keyframeEx.addEventListener('click', (e) => {
+  e.target.classList.toggle('slide');
+});
+
+let oneAtATimeContainers = [
+  ...document.querySelectorAll('.oneAtATimeContainers'),
+];
+let oneAtATimeButtons = document.querySelectorAll('.oneAtATimeButtons');
+
+oneAtATimeContainers.forEach((container) =>
+  container.addEventListener('click', (event) => {
+    let container = event.target;
+    let children = [...container.children];
+    let next = children.find((el) => el.style.opacity == 0);
+    next.style.opacity = 1;
+  })
+);
+
+let makeBlock = document.querySelector('.makeBlock');
+
+makeBlock.addEventListener('click', (e) => {
+  console.log(e.target.previousElementSibling);
+  e.target.previousElementSibling.classList.toggle('block');
+});
+
+let toggleFlex = document.querySelector('.toggleFlex');
+toggleFlex.addEventListener('click', (e) => {
+  console.log(e.target.previousElementSibling);
+  console.log(e.target.previousElementSibling.classList);
+  e.target.previousElementSibling.classList.toggle('flex');
+  let word = e.target.previousElementSibling.classList.contains('flex')
+    ? 'flex'
+    : 'block';
+  e.target.nextElementSibling.innerText = `display is ${word}`;
+});
+
+let marginController = document.querySelectorAll('.margin-Controller');
+
+marginController.forEach((one) =>
+  one.addEventListener('change', (e) => {
+    let marginbox = document.querySelector('.marginbox');
+    let val = e.target.value;
+    let prop = e.target.name;
+    marginbox.style[prop] = val;
+  })
+);
+
+let paddingController = document.querySelectorAll('.padding-Controller');
+
+paddingController.forEach((one) =>
+  one.addEventListener('change', (e) => {
+    let paddingbox = document.querySelector('.paddingbox');
+    let val = e.target.value;
+    let prop = e.target.name;
+    paddingbox.style[prop] = val;
+  })
+);
+
+// let showAllOneAtATime = document.querySelectorAll('.showAll');
+// showAllOneAtATime.forEach(showAll => showAll.addEventListener('click', (e) => {
+
+// })
